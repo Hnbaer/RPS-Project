999f73b0b3c4e5bdfb9a0bf62cb57d53a711f251
flaredshadow <flaredlight@aim.com>
2016-07-23 15:06:49 -0700

    First Major Push.  Andrew : Gameplay, Sam : Menu, Max : Networking

---
+++ RPS Project/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName("MainMenuScene"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void _goToScene(string givenSceneName)
+	{
+		AudioSource audioSource = Engine.self.GetComponent<AudioSource>();
+		audioSource.clip = Engine.self.song1;
+		audioSource.Play();
+		SceneManager.LoadScene(givenSceneName, LoadSceneMode.Additive);
+		SceneManager.UnloadScene("MainMenuScene");
+	}
+}
---
+++ RPS Project/Assets/Scripts/Engine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public enum GameState {InitGame, MainMenu, MatchMaking, Options, Playing}
+public enum PlayState {InitPlay, Play, Win, Lose, Quit }
+public class Engine : MonoBehaviour {
+
+	public static Engine self;
+
+	public AudioClip song1;
+
+	GameState currentGameState;
+	string mainSceneName = "MainMenuScene";
+
+	// Use this for initialization
+	void Start () {
+		self = this;
+		currentGameState = GameState.InitGame;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(currentGameState)
+		{
+			case GameState.InitGame:
+				SceneManager.LoadScene(mainSceneName, LoadSceneMode.Additive);
+				//To Do : Query Database
+				currentGameState = GameState.MainMenu;
+				break;
+			case GameState.MainMenu:
+				break;
+			case GameState.MatchMaking:
+				//To Do : Query For Players currently playing the game
+				break;
+			case GameState.Options:
+				break;
+			case GameState.Playing:
+				break;
+		}
+	
+	}
+}
